813ee9c20c7e93d70ff44986dc504a3a0719a492
Paras Mehta <paras.mehta@veersatech.com>
2025-07-30 23:33:44 +0530

    react components created

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./Container/container";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer };
+
+export { Header, Footer, Logo, Container, LogoutBtn };
